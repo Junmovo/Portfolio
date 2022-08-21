92a194f95bdfc3544d2e83030cf990651c519687
Junmovo <maneya71@naver.com>
2022-08-21 16:57:09 +0900

    맥딜리버리 추가

---
+++ subpage/js/mcdelivery.js
@@ -0,0 +1,92 @@
+AOS.init();
+
+// $('.dumitxt02 li:nth-child(1)').on('mouseenter',()=>{
+  
+//   $('.dumitxt02 li:nth-child(1) p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+//   $('.longline').css({'height':'665px'})
+// })
+// $('.dumitxt02 li:nth-child(1)').on('mouseleave',()=>{
+//   $('.dumitxt02 li:nth-child(1) p').stop().css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+//   $('.longline').css({'height':'620px'})
+// })
+
+$('.dumitxt02 li:nth-child(1)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(1)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(2)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(2)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(3)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(3)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(4)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(4)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(5)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '40px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(5)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(6)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(6)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(7)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(7)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(8)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(8)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(9)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '30px', 'margin-top':'20px'})
+  $('.longline').css({'height':'665px'})
+})
+$('.dumitxt02 li:nth-child(9)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
+$('.dumitxt02 li:nth-child(10)').mouseenter(function(){
+  $(this).find('p').css({'width':'100%', 'height' : '40px', 'margin-top':'20px'})
+ 
+})
+$('.dumitxt02 li:nth-child(10)').mouseleave(function(){
+  $(this).find('p').css({'width':'100%','height' : '0px' , 'margin-top':'0px'})
+  $('.longline').css({'height':'620px'})
+})
