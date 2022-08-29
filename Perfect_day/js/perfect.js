03066b455dcca4dcf000ede30cd22817c4ec2c9b
Junmovo <maneya71@naver.com>
2022-08-29 12:31:51 +0900

    [220829] 테블릿 반응형 추가

--- Perfect_day/js/perfect.js
+++ Perfect_day/js/perfect.js
@@ -15,3 +15,43 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 AOS.init();
+
+$('.selectbox ul li a').eq(0).on('click',function(e){
+  e.preventDefault();
+  $('.selectbox ul li a').removeClass('active')
+  $('.selectbox ul li a').eq(0).addClass('active')
+  $('.bestbox').addClass('on')
+  $('.seoul').removeClass('on')
+  $('.gyong').removeClass('on')
+  $('.country_all').removeClass('on')
+ })
+$('.selectbox ul li a').eq(1).on('click',function(e){
+  e.preventDefault();
+  $('.selectbox ul li a').removeClass('active')
+  $('.selectbox ul li a').eq(1).addClass('active')
+  $('.bestbox').removeClass('on')
+  $('.seoul').addClass('on')
+  $('.gyong').removeClass('on')
+  $('.country_all').removeClass('on')
+ })
+$('.selectbox ul li a').eq(2).on('click',function(e){
+  e.preventDefault();
+  $('.selectbox ul li a').removeClass('active')
+  $('.selectbox ul li a').eq(2).addClass('active')
+  $('.bestbox').removeClass('on')
+  $('.seoul').removeClass('on')
+  $('.gyong').addClass('on')
+  $('.country_all').removeClass('on')
+ })
+$('.selectbox ul li a').eq(3).on('click',function(e){
+  e.preventDefault();
+  $('.selectbox ul li a').removeClass('active')
+  $('.selectbox ul li a').eq(3).addClass('active')
+  $('.bestbox').removeClass('on')
+  $('.seoul').removeClass('on')
+  $('.gyong').removeClass('on')
+  $('.country_all').addClass('on')
+ })
+
+
+
